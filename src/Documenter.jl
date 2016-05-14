560ec63b475f2efbf457c716860806f0c5f93cf3
Michael Hatherly <michaelhatherly@gmail.com>
2016-05-14 22:07:19 +0200

    Move debug code.

--- src/Documenter.jl
+++ src/Documenter.jl
@@ -228,16 +228,16 @@ function deploydocs(;
             travis_tag    != ""
         )
 
-    # When env variable `DOCUMENTER_DEBUG == "true"` then print some debugging info.
-    debugdeploy(
-        github_api_key,
-        travis_repo_slug,
-        travis_pull_request,
-        travis_osname,
-        travis_julia,
-        travis_branch,
-        travis_tag,
-    )
+    if get(ENV, "DOCUMENTER_DEBUG", "") == "true"
+        Utilities.debug("GITHUB_API_KEY empty = $(github_api_key == "")")
+        Utilities.debug("TRAVIS_REPO_SLUG     = \"$travis_repo_slug\"")
+        Utilities.debug("TRAVIS_PULL_REQUEST  = \"$travis_pull_request\"")
+        Utilities.debug("TRAVIS_OS_NAME       = \"$travis_osname\"")
+        Utilities.debug("TRAVIS_JULIA_VERSION = \"$travis_julia\"")
+        Utilities.debug("TRAVIS_BRANCH        = \"$travis_branch\"")
+        Utilities.debug("TRAVIS_TAG           = \"$travis_tag\"")
+        Utilities.debug("should_deploy        = \"$should_deploy\"")
+    end
 
     if should_deploy
         # Add local bin path if needed.
@@ -290,26 +290,6 @@ function deploydocs(;
         end
     else
         Utilities.log("skipping docs deployment.")
-    end
-end
-
-function debugdeploy(
-        github_api_key,
-        travis_repo_slug,
-        travis_pull_request,
-        travis_osname,
-        travis_julia,
-        travis_branch,
-        travis_tag,
-    )
-    if get(ENV, "DOCUMENTER_DEBUG", "") == "true"
-        Utilities.debug("GITHUB_API_KEY empty = $(github_api_key == "")")
-        Utilities.debug("TRAVIS_REPO_SLUG     = \"$travis_repo_slug\"")
-        Utilities.debug("TRAVIS_PULL_REQUEST  = \"$travis_pull_request\"")
-        Utilities.debug("TRAVIS_OS_NAME       = \"$travis_osname\"")
-        Utilities.debug("TRAVIS_JULIA_VERSION = \"$travis_julia\"")
-        Utilities.debug("TRAVIS_BRANCH        = \"$travis_branch\"")
-        Utilities.debug("TRAVIS_TAG           = \"$travis_tag\"")
     end
 end
 
