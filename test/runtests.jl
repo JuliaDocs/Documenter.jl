f1b03dfcbe062bdef1bece5b0c31bdcbb244d432
Morten Piibeleht <morten.piibeleht@gmail.com>
2016-10-02 23:24:21 +0200

    Move Utilities tests to a sep. file, use testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,51 +19,7 @@ using Compat
 
 # Unit tests for module internals.
 
-module UnitTests
-    module SubModule end
-
-    # Does `submodules` collect *all* the submodules?
-    module A
-        module B
-            module C
-                module D end
-            end
-        end
-    end
-
-    type T end
-
-    "Documenter unit tests."
-    Base.length(::T) = 1
-end
-
-let doc = @doc(length)
-    a = Documenter.Utilities.filterdocs(doc, Set{Module}())
-    b = Documenter.Utilities.filterdocs(doc, Set{Module}([UnitTests]))
-    c = Documenter.Utilities.filterdocs(doc, Set{Module}([Base]))
-    d = Documenter.Utilities.filterdocs(doc, Set{Module}([Tests]))
-
-    @test !isnull(a)
-    @test get(a) === doc
-    @test !isnull(b)
-    @test contains(stringmime("text/plain", get(b)), "Documenter unit tests.")
-    @test !isnull(c)
-    @test !contains(stringmime("text/plain", get(c)), "Documenter unit tests.")
-    @test isnull(d)
-end
-
-# Documenter.Utilities.issubmodule
-@test Documenter.Utilities.issubmodule(Main, Main) === true
-@test Documenter.Utilities.issubmodule(UnitTests, UnitTests) === true
-@test Documenter.Utilities.issubmodule(UnitTests.SubModule, Main) === true
-@test Documenter.Utilities.issubmodule(UnitTests.SubModule, UnitTests) === true
-@test Documenter.Utilities.issubmodule(UnitTests.SubModule, Base) === false
-@test Documenter.Utilities.issubmodule(UnitTests, UnitTests.SubModule) === false
-
-@test UnitTests.A in Documenter.Utilities.submodules(UnitTests.A)
-@test UnitTests.A.B in Documenter.Utilities.submodules(UnitTests.A)
-@test UnitTests.A.B.C in Documenter.Utilities.submodules(UnitTests.A)
-@test UnitTests.A.B.C.D in Documenter.Utilities.submodules(UnitTests.A)
+include("utilities.jl")
 
 ## NavNode tests
 
