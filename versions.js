b768cff8c0ade311e385e5be9364d0722da74fca
autodocs <autodocs>
2016-11-19 17:51:17 +0000

    build based on 271394e

--- versions.js
+++ versions.js
@@ -1,6 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
   "latest",
+  "release-0.8",
   "release-0.7",
   "release-0.6",
   "release-0.5",
@@ -8,6 +9,7 @@ var DOC_VERSIONS = [
   "release-0.3",
   "release-0.2",
   "release-0.1",
+  "v0.8.0",
   "v0.7.2",
   "v0.7.1",
   "v0.7.0",
