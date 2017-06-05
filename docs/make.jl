edb6803d5af1afe505164a62e1685b081d26995f
Morten Piibeleht <morten.piibeleht@gmail.com>
2017-06-05 17:06:31 +1200

    Remove old files before copy in deploy (#510)
    This way we also commit case changes in file names on filesystems that
    are case-insensitive.
    
    Fixes #507

--- docs/make.jl
+++ docs/make.jl
@@ -28,6 +28,7 @@ makedocs(
                 "lib/internals/cross-references.md",
                 "lib/internals/docchecks.md",
                 "lib/internals/docsystem.md",
+                "lib/internals/documenter.md",
                 "lib/internals/documents.md",
                 "lib/internals/dom.md",
                 "lib/internals/expanders.md",
---
+++ docs/src/lib/internals/documenter.md
@@ -0,0 +1,5 @@
+# Documenter
+
+```@docs
+Documenter.gitrm_copy
+```
