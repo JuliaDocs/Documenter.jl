7c52ef72426b9dc5131d9e1d26101c8c5bc921f7
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-06-08 11:01:10 +0200

    Create LaTeXWriter.jl

--- src/Writers/LaTeXWriter.jl
+++ src/Writers/LaTeXWriter.jl
@@ -260,7 +260,7 @@ const LEXER = Set([
 ])
 
 function latex(io::IO, code::Markdown.Code)
-    language = if startswith(code.language, "jldoctest")
+    language = if first(split(c.language)) == "jldoctest"
         # When the doctests are not being run, Markdown.Code blocks will have jldoctest as
         # the language attribute. The check here to determine if it is a REPL-type or
         # script-type doctest should match the corresponding one in DocChecks.jl. This makes
