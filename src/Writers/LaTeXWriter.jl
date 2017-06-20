1e55a78b29f7350569aae47021a1e3e8d32c1156
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2017-06-20 02:08:26 -0700

    Detect named doctests as jldoctest (#513)

--- src/Writers/LaTeXWriter.jl
+++ src/Writers/LaTeXWriter.jl
@@ -260,7 +260,9 @@ const LEXER = Set([
 ])
 
 function latex(io::IO, code::Markdown.Code)
-    language = if code.language == "jldoctest"
+    language = if isempty(code.language)
+          "none"
+    elseif first(split(code.language)) == "jldoctest"
         # When the doctests are not being run, Markdown.Code blocks will have jldoctest as
         # the language attribute. The check here to determine if it is a REPL-type or
         # script-type doctest should match the corresponding one in DocChecks.jl. This makes
