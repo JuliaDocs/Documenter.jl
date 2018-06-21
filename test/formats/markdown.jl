c9b66eccdf9080770dda27b2a4663115fc1037f3
Morten Piibeleht <morten.piibeleht@gmail.com>
2018-06-21 19:21:22 +0200

    Fix Markdown format test
    Use correct capitalization for the assets/mkdocs/Documenter.css file.
    The needed a bit of reorganization -- it was working before because it
    was building into a dirty docs/build directory. Now the Markdown build
    goes into a completely different directory. Also, no need to remove the
    output files, instead let's clean the directory when the build starts.

--- test/formats/markdown.jl
+++ test/formats/markdown.jl
@@ -8,18 +8,19 @@ using Documenter
 
 # Documenter package docs
 @info("Building Documenter's docs with Markdown.")
-const Documenter_root = normpath(joinpath(dirname(@__FILE__), "..", "..", "docs"))
+const Documenter_root = normpath(joinpath(@__DIR__, "..", "..", "docs"))
+build_dir_relpath = relpath(joinpath(@__DIR__, "builds/markdown"), Documenter_root)
 doc = makedocs(
     debug   = true,
     root    = Documenter_root,
     modules = Documenter,
-    clean   = false,
+    build   = build_dir_relpath,
 )
 
 @testset "Markdown" begin
     @test isa(doc, Documenter.Documents.Document)
 
-    let build_dir  = joinpath(Documenter_root, "build"),
+    let build_dir  = joinpath(Documenter_root, build_dir_relpath),
         source_dir = joinpath(Documenter_root, "src")
         @test isdir(build_dir)
         @test isdir(joinpath(build_dir, "assets"))
@@ -28,15 +29,13 @@ doc = makedocs(
 
         @test isfile(joinpath(build_dir, "index.md"))
         @test isfile(joinpath(build_dir, "assets", "mathjaxhelper.js"))
-        @test isfile(joinpath(build_dir, "assets", "documenter.css"))
+        @test isfile(joinpath(build_dir, "assets", "Documenter.css"))
     end
 
     @test doc.user.root   == Documenter_root
     @test doc.user.source == "src"
-    @test doc.user.build  == "build"
-    @test doc.user.clean  == false
-
-    rm(joinpath(Documenter_root, "build"); recursive = true)
+    @test doc.user.build  == build_dir_relpath
+    @test doc.user.clean  == true
 
     mktempdir() do root
         let path = joinpath(root, "docs")
