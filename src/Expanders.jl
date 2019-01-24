2add0e6e262565290af98f451afb4e869820ec89
Fredrik Ekre <fredrik.ekre@outlook.com>
2019-01-24 11:22:24 +0100

    fix #886: skip reserved words special casing in code-block parsing. (#927)

--- src/Expanders.jl
+++ src/Expanders.jl
@@ -477,7 +477,7 @@ function Selectors.runner(::Type{EvalBlocks}, x, page, doc)
     lines = Utilities.find_block_in_file(x.code, page.source)
     cd(dirname(page.build)) do
         result = nothing
-        for (ex, str) in Utilities.parseblock(x.code, doc, page)
+        for (ex, str) in Utilities.parseblock(x.code, doc, page; keywords = false)
             try
                 result = Core.eval(sandbox, ex)
             catch err
@@ -535,7 +535,7 @@ function Selectors.runner(::Type{ExampleBlocks}, x, page, doc)
         else
             code = x.code
         end
-        for (ex, str) in Utilities.parseblock(code, doc, page)
+        for (ex, str) in Utilities.parseblock(code, doc, page; keywords = false)
             (value, success, backtrace, text) = Utilities.withoutput() do
                 cd(dirname(page.build)) do
                     Core.eval(mod, Expr(:(=), :ans, ex))
@@ -598,7 +598,7 @@ function Selectors.runner(::Type{REPLBlocks}, x, page, doc)
     mod  = get!(() -> get_new_sandbox(sym), page.globals.meta, sym)
     code = split(x.code, '\n'; limit = 2)[end]
     result, out = nothing, IOBuffer()
-    for (ex, str) in Utilities.parseblock(x.code, doc, page)
+    for (ex, str) in Utilities.parseblock(x.code, doc, page; keywords = false)
         buffer = IOBuffer()
         input  = droplines(str)
         (value, success, backtrace, text) = Utilities.withoutput() do
