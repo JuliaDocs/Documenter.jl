77c121fa79a2e76e4cf077c8b25ff4022708b398
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-03-28 19:01:10 +1300

    Display stdout if nothing result in at-example (#987)

--- src/Expanders.jl
+++ src/Expanders.jl
@@ -572,7 +572,12 @@ function Selectors.runner(::Type{ExampleBlocks}, x, page, doc)
 
     # Only add content when there's actually something to add.
     isempty(input)  || push!(content, Markdown.Code("julia", input))
-    isempty(output) || push!(content, output)
+    if result === nothing
+        code = Documenter.DocTests.sanitise(buffer)
+        isempty(code) || push!(content, Markdown.Code(code))
+    elseif !isempty(output)
+        push!(content, output)
+    end
     # ... and finally map the original code block to the newly generated ones.
     page.mapping[x] = Documents.MultiOutput(content)
 end
