e320037ebc6c88244aacc35283595172e72a9b4e
Kevin Bonham <kevbonham@gmail.com>
2019-05-23 13:47:43 -0400

    don't inlclude build dir

--- test/workdir/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"test/#Testing-1","page":"Home","title":"Testing","text":"","category":"section"},{"location":"test/#","page":"Home","title":"Home","text":"Here's a test","category":"page"},{"location":"test/#","page":"Home","title":"Home","text":"pwd()","category":"page"}]
-}
