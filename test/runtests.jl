bc819926c48fbd6b46a5b0ff9770df2fde714a83
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-08-15 10:28:56 +1200

    Move highlightsig! tests to expanders.jl
    This way we now have everything in a single testset again.

--- test/runtests.jl
+++ test/runtests.jl
@@ -41,6 +41,9 @@ println("="^50)
     # MDFlatten tests.
     include("mdflatten.jl")
 
+    # Expanders
+    include("expanders.jl")
+
     # HTMLWriter
     include("htmlwriter.jl")
 
@@ -58,38 +61,7 @@ println("="^50)
 
     # Passing a writer positionally (https://github.com/JuliaDocs/Documenter.jl/issues/1046)
     @test_throws ArgumentError makedocs(sitename="", HTML())
+
+    # Running doctest() on our own manual
+    include("manual.jl")
 end
-
-# Additional tests
-# Docstring signature syntax highlighting tests.
-module HighlightSig
-    using Test
-    import Markdown
-    import Documenter.Expanders: highlightsig!
-
-    @testset "highlightsig!" begin
-        s = """
-                foo(bar::Baz)
-            ---
-                foo(bar::Baz)
-            """
-        original = Markdown.parse(s)
-        md = Markdown.parse(s)
-        highlightsig!(md)
-        @test isempty(original.content[1].language)
-        @test md.content[1].language == "julia"
-        @test original.content[end].language == md.content[end].language
-
-        s = """
-            ```lang
-             foo(bar::Baz)
-            ```
-            """
-        original = Markdown.parse(s)
-        md = Markdown.parse(s)
-        highlightsig!(md)
-        @test original == md
-    end
-end
-
-include("manual.jl")
