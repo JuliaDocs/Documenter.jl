7b55cab6871611b1a58d88e0b70d8b33632f8e63
Morten Piibeleht <morten.piibeleht@gmail.com>
2020-03-05 10:53:31 +1300

    Update docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -35,7 +35,7 @@ makedocs(
             hide("man/hosting.md", [
                 "man/hosting/walkthrough.md"
             ]),
-            "man/theming.md"
+            "man/theming.md",
             "man/other-formats.md",
         ],
         "showcase.md",
