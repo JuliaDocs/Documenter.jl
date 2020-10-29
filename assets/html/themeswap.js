0af86d779647735e52cf9fdf97d2b82eb6c96d2d
Abhinav Kaushlya <abhinavkaushlya@gmail.com>
2020-10-29 22:23:52 +0530

    Respect user preference for color scheme

--- assets/html/themeswap.js
+++ assets/html/themeswap.js
@@ -5,6 +5,14 @@ function set_theme_from_local_storage() {
   // Get the user-picked theme from localStorage. May be `null`, which means the default
   // theme.
   var theme =  window.localStorage.getItem("documenter-theme");
+  // Check if the browser supports user color preference
+  var darkPreference = false;
+  if(window.matchMedia('(prefers-color-scheme: dark)').media !== 'not all') {
+    // Check if the users preference is for dark color scheme
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches === true) {
+      darkPreference = true;
+    }
+  }
   // Initialize a few variables for the loop:
   //
   //  - active: will contain the index of the theme that should be active. Note that there
@@ -14,7 +22,7 @@ function set_theme_from_local_storage() {
   //
   //  - disabled: style sheets that should be disabled (i.e. all the theme style sheets
   //    that are not the currently active theme)
-  var active = null; var disabled = [];
+  var active = null; var disabled = []; var darkTheme = null;
   for (var i = 0; i < document.styleSheets.length; i++) {
     var ss = document.styleSheets[i];
     // The <link> tag of each style sheet is expected to have a data-theme-name attribute
@@ -25,8 +33,10 @@ function set_theme_from_local_storage() {
     // To distinguish the default (primary) theme, it needs to have the data-theme-primary
     // attribute set.
     var isprimary = (ss.ownerNode.getAttribute("data-theme-primary") !== null);
+    // If the ss is not primary then use it if the user's preference is towards dark colors
+    if(!isprimary) darkTheme = themename;
     // If we find a matching theme (and it's not the default), we'll set active to non-null
-    if(!isprimary && themename === theme) active = i;
+    if(themename === theme) active = i;
     // Store the style sheets of inactive themes so that we could disable them
     if(themename !== theme) disabled.push(ss);
   }
@@ -37,6 +47,22 @@ function set_theme_from_local_storage() {
     disabled.forEach(function(ss){
       ss.disabled = true;
     });
+  }
+  else if(darkTheme !== null) {
+    // If we did find an active theme, we'll (1) add the theme--$(theme) class to <html>
+    document.getElementsByTagName('html')[0].className = "theme--" + darkTheme;
+    // and (2) disable all the other theme stylesheets
+    disabled.forEach(function(ss){
+      if (ss.ownerNode.getAttribute("data-theme-name") !== darkTheme) {
+        ss.disabled = true;
+      }
+    });
+    // Store the theme in localStorage
+    if(typeof(window.localStorage) !== "undefined") {
+      window.localStorage.setItem("documenter-theme", darkTheme);
+    } else {
+      console.error("Browser does not support window.localStorage");
+    }
   }
 }
 set_theme_from_local_storage();
