d8cddd5cbcfc6f0ae3c02ac1ca397f3fb97f64ae
Morten Piibeleht <morten.piibeleht@gmail.com>
2021-03-23 19:39:42 +1300

    Fixes for TestUtilities
    Support at-quietly on testsets better and fix a variable name that was
    forgotten when IOCapture was bumped.

--- test/TestUtilities.jl
+++ test/TestUtilities.jl
@@ -25,20 +25,17 @@ function _quietly(f, expr, source)
         println(io, "@quietly: $(source.file):$(source.line)")
         println(io, "@quietly: typeof(result) = ", typeof(c.value))
         println(io, "@quietly: STDOUT")
-        println(io, output)
+        println(io, c.output)
         println(io, "@quietly: end of STDOUT")
-        if success
-            println(io, "@quietly: result =")
-            println(io, c.value)
-        else
+        if c.error
             println(io, "@quietly: result (error) =")
             showerror(io, c.value, c.backtrace)
+        else
+            println(io, "@quietly: result =")
+            println(io, c.value)
         end
     end
-    if !c.error
-        printstyled("@quietly: success, $(sizeof(c.output)) bytes of output hidden\n"; color=:magenta)
-        return c.value
-    else
+    if c.error
         @error """
         An error was thrown in @quietly, $(sizeof(c.output)) bytes of output captured
         $(typeof(c.value)) at $(source.file):$(source.line) in expression:
@@ -51,6 +48,22 @@ function _quietly(f, expr, source)
             printstyled("$("="^27) @quietly: end of output $("="^28)\n"; color=:magenta)
         end
         throw(QuietlyException(c.value, c.backtrace))
+    elseif c.value isa Test.DefaultTestSet && !is_success(c.value)
+        @error """
+        @quietly: a testset with failures, $(sizeof(c.output)) bytes of output captured
+        $(typeof(c.value)) at $(source.file):$(source.line) in expression:
+        $(expr)
+        """ TestSet = c.value
+        if !isempty(c.output)
+            printstyled("$("="^21) @quietly: output from the expression $("="^21)\n"; color=:magenta)
+            print(c.output)
+            last(c.output) != "\n" && println()
+            printstyled("$("="^27) @quietly: end of output $("="^28)\n"; color=:magenta)
+        end
+        return c.value
+    else
+        printstyled("@quietly: success, $(sizeof(c.output)) bytes of output hidden\n"; color=:magenta)
+        return c.value
     end
 end
 macro quietly(expr)
@@ -61,6 +74,15 @@ macro quietly(expr)
             $(esc(expr))
         end
     end
+end
+
+is_success(testset::Test.DefaultTestSet) = !(testset.anynonpass || !is_success(testset.results))
+is_success(ts::AbstractArray) = all(is_success.(ts))
+is_success(::Test.Fail) = false
+is_success(::Test.Pass) = true
+function is_success(x)
+    @warn "Unimplemented TestUtilities.is_success method" typeof(x) x
+    return false
 end
 
 end
