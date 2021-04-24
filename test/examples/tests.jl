16352d87758e26205419426806f44bf268ec79c3
Morten Piibeleht <morten.piibeleht@gmail.com>
2021-04-24 15:36:11 +1200

    Fix tests

--- test/examples/tests.jl
+++ test/examples/tests.jl
@@ -128,7 +128,7 @@ end
 
         @test realpath(doc.internal.assets) == realpath(joinpath(dirname(@__FILE__), "..", "..", "assets"))
 
-        @test length(doc.blueprint.pages) == 19
+        @test length(doc.blueprint.pages) == 20
 
         let headers = doc.internal.headers
             @test Documenter.Anchors.exists(headers, "Documentation")
