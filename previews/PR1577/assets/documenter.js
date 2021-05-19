1a302e3595bc6c0477bfecbfb7be90fecf6f8bb1
Documenter.jl <documenter@juliadocs.github.io>
2021-05-19 10:42:35 +0000

    build based on 0be68c3

--- previews/PR1577/assets/documenter.js
+++ previews/PR1577/assets/documenter.js
@@ -291,16 +291,14 @@ function maybeAddWarning () {
         };
 
         const div = document.createElement('div');
-        div.setAttribute('style', 'position: fixed; width: 100%; top: 0; left: 0; box-shadow: 0 0 10px rgba(0,0,0,0.3); z-index: 999; background-color: #ffaf9c; color: rgba(0, 0, 0, 0.7); border-bottom: 1px solid #d54625; padding: 10px 35px; text-align: center; font-size: 15px;');
-        const closer = document.createElement('div');
-        closer.setAttribute('style', 'position: absolute; top: calc(50% - 8px); right: 18px; cursor: pointer; width: 12px;');
-        // Icon by font-awesome (license: https://fontawesome.com/license, link: https://fontawesome.com/icons/times?style=solid)
-        closer.innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>';
+        div.classList.add('outdated-warning-overlay');
+        const closer = document.createElement('button');
+        closer.classList.add('outdated-warning-closer', 'delete');
         closer.addEventListener('click', function () {
             document.body.removeChild(div);
         });
         const href = window.documenterBaseURL + '/../' + window.DOCUMENTER_NEWEST;
-        div.innerHTML = 'This is an old version of the documentation. <br> <a href="' + href + '" style="color: green">Go to the newest version</a>.';
+        div.innerHTML = 'This is an old version of the documentation. <br> <a href="' + href + '">Go to the newest version</a>.';
         div.appendChild(closer);
         document.body.appendChild(div);
     };
