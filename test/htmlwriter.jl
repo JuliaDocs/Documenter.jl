1672c02522d6350bf403b3121324789ab0f25edd
odow <o.dowson@gmail.com>
2021-07-05 08:48:07 +1200

    Fix escaping of dollar symbols in HTMLWriter
    Previously, Javascript LaTeX engines got confused trying to
    distinguish between inline delimiters and dollar symbols for actual
    currency. This works around the problem by wrapping (currency) dollar
    symbols in a span tag.

--- test/htmlwriter.jl
+++ test/htmlwriter.jl
@@ -171,5 +171,10 @@ end
             end
         end
     end
+
+    @test "Dollar escapes" begin
+        @test string(HTMLWriter.mdconvert("\$1")) == "\$1"
+        @test string(HTMLWriter.mdconvert("\$")) == "<span>\$</span>"
+    end
 end
 end
