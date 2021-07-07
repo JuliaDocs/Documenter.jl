4d54ebe59966ed441eb1d9a6d67b84ea07166ddc
Documenter.jl <documenter@juliadocs.github.io>
2021-07-07 01:13:44 +0000

    build based on 497be4a

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -73,8 +73,6 @@ $(document).ready(function() {
 ////////////////////////////////////////////////////////////////////////////////
 require(['jquery', 'highlight', 'highlight-julia', 'highlight-julia-repl', 'highlight-yaml'], function($) {
 $(document).ready(function() {
-    hljs.configure({ignoreUnescapedHTML: true}); // for ANSI color support
-    /* TODO: add a plugin for unescaped HTML */
     hljs.highlightAll();
 })
 
