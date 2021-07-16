f50ed560e4f84c854e60af96b3ed95fb777fa340
Morten Piibeleht <morten.piibeleht@gmail.com>
2021-07-16 15:34:03 +0000

    build based on 23c1608

--- versions.js
+++ versions.js
@@ -1,9 +1,13 @@
 var DOC_VERSIONS = [
   "stable",
-  "v1.2",
+  "v0.27",
+  "v0.26",
+  "v0.25",
   "v0.24",
   "v0.23",
   "v0.22",
   "v0.21",
   "dev",
 ];
+var DOCUMENTER_NEWEST = "v0.27.3";
+var DOCUMENTER_STABLE = "stable";
