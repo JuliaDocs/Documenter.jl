3b7a998ce504f4b017cd372b99f1b82a9988217b
Documenter.jl <documenter@juliadocs.github.io>
2022-01-12 16:56:24 +0000

    build based on 4d3ebcd

--- previews/PR1748/assets/documenter.js
+++ previews/PR1748/assets/documenter.js
@@ -79,58 +79,27 @@ $(document).ready(function() {
 })
 ////////////////////////////////////////////////////////////////////////////////
 require([], function() {
-function unescape_str(str) {
-  return str.replace(/\\(.)/g, function (match, val) {
-    let out;
-    switch (val) {
-      case "a":
-        out = "a";
-        break;
-      case "b":
-        out = "\b";
-        break;
-      case "t":
-        out = "\t";
-        break;
-      case "n":
-        out = "\n";
-        break;
-      case "v":
-        out = "\v";
-        break;
-      case "f":
-        out = "\f";
-        break;
-      case "r":
-        out = "\r";
-        break;
-      case "e":
-        out = "e";
-        break;
-      default:
-        out = val;
-    }
-    return out;
-  });
-}
-
 function addCopyButtonCallbacks() {
-  for (const el of document.getElementsByClassName("copy-button")) {
-    el.addEventListener("click", function () {
-      navigator.clipboard.writeText(unescape_str(el.dataset.value)).then(
+  for (const el of document.getElementsByTagName("pre")) {
+    const button = document.createElement("button");
+    button.classList.add("copy-button", "fas", "fa-copy");
+    el.appendChild(button);
+
+    button.addEventListener("click", function () {
+      navigator.clipboard.writeText(el.innerText).then(
         function () {
-          el.classList.add("success", "fa-check")
-          el.classList.remove("fa-copy")
+          button.classList.add("success", "fa-check");
+          button.classList.remove("fa-copy");
         },
         function () {
-          el.classList.add("error", "fa-times")
-          el.classList.remove("fa-copy")
+          button.classList.add("error", "fa-times");
+          button.classList.remove("fa-copy");
         }
       );
 
       setTimeout(function () {
-        el.classList.add("fa-copy")
-        el.classList.remove("success", "fa-check", "fa-times")
+        button.classList.add("fa-copy");
+        button.classList.remove("success", "fa-check", "fa-times");
       }, 5000);
     });
   }
