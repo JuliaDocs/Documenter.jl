57b89df0ed825cdde717134d2c239734496045d7
Sebastian Pfitzner <pfitzseb@gmail.com>
2022-01-17 12:07:31 +0100

    improve copy button JS

--- assets/html/js/copy.js
+++ assets/html/js/copy.js
@@ -4,17 +4,22 @@ function addCopyButtonCallbacks() {
     button.classList.add("copy-button", "fas", "fa-copy");
     el.appendChild(button);
 
+    const success = function () {
+      button.classList.add("success", "fa-check");
+      button.classList.remove("fa-copy");
+    };
+
+    const failure = function () {
+      button.classList.add("error", "fa-times");
+      button.classList.remove("fa-copy");
+    };
+
     button.addEventListener("click", function () {
-      navigator.clipboard.writeText(el.innerText).then(
-        function () {
-          button.classList.add("success", "fa-check");
-          button.classList.remove("fa-copy");
-        },
-        function () {
-          button.classList.add("error", "fa-times");
-          button.classList.remove("fa-copy");
-        }
-      );
+      if (window.navigator && window.navigator.clipboard && window.navigator.clipboard.writeText) {
+        window.navigator.clipboard.writeText(el.innerText).then(success, failure);
+      } else {
+        failure()
+      }
 
       setTimeout(function () {
         button.classList.add("fa-copy");
