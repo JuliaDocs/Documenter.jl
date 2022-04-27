dee8d8eb23697caaf9c1d957502ddcddc6e30d0b
Viral B. Shah <ViralBShah@users.noreply.github.com>
2022-04-27 18:47:45 -0400

    Update assets/html/warner.js
    Co-authored-by: Morten Piibeleht <morten.piibeleht@gmail.com>

--- assets/html/warner.js
+++ assets/html/warner.js
@@ -37,7 +37,7 @@ function maybeAddWarning () {
         document.body.removeChild(div);
     });
     const href = window.documenterBaseURL + '/../' + window.DOCUMENTER_STABLE;
-    div.innerHTML = 'This documentation is for the development version.<br> <a href="' + href + '">Go to the documentation for the latest stable release</a>.';
+    div.innerHTML = 'This documentation is not for the latest stable release, but for either the development version or an older outdated release.<br><a href="' + href + '">Click here to go to the documentation for the latest stable release.</a>';
     div.appendChild(closer);
     document.body.appendChild(div);
 };
