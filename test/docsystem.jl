7a81017863b5bfd87b846cf864df1806512a1ebb
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-06-11 15:05:40 +1200

    Reorganize tests a bit
    * stick to Documenter's binding, for consistency (at-var is not
      documented)
    * Be explicit about the type signature
    * Add, rather than replace tests
    * Add separate test with the example from PR (also, covers the case of
      types)

--- test/docsystem.jl
+++ test/docsystem.jl
@@ -3,9 +3,16 @@ module DocSystemTests
 using Test
 
 import Documenter: Documenter, DocSystem
-import Base.Docs: Binding, @var
 
 const alias_of_getdocs = DocSystem.getdocs # NOTE: won't get docstrings if in a @testset
+
+module DocstringAliasTest
+"A"
+struct A end
+"A(x)"
+A(x) = A()
+B = A
+end
 
 @testset "DocSystem" begin
     ## Bindings.
@@ -43,14 +50,14 @@ const alias_of_getdocs = DocSystem.getdocs # NOTE: won't get docstrings if in a 
     @test (fieldnames(DocSystem.DocStr)...,) == (:text, :object, :data)
     ## `getdocs`.
     let b   = DocSystem.binding(DocSystem, :getdocs),
-        getdocs_signature = Union{Tuple{Binding}, Tuple{Binding, Type}},
         d_0 = DocSystem.getdocs(b, Tuple{}),
         d_1 = DocSystem.getdocs(b),
-        d_2 = DocSystem.getdocs(b, getdocs_signature; compare = (==)),
+        d_2 = DocSystem.getdocs(b, Union{Tuple{Any}, Tuple{Any, Type}}; compare = (==)),
         d_3 = DocSystem.getdocs(b; modules = Module[Main]),
-        d_4 = DocSystem.getdocs(@var(alias_of_getdocs)),
-        d_5 = DocSystem.getdocs(@var(alias_of_getdocs); aliases = false),
-        d_6 = DocSystem.getdocs(@var(alias_of_getdocs), getdocs_signature)
+        d_4 = DocSystem.getdocs(DocSystem.binding(@__MODULE__, :alias_of_getdocs)),
+        d_5 = DocSystem.getdocs(DocSystem.binding(@__MODULE__, :alias_of_getdocs); aliases = false),
+        d_6 = DocSystem.getdocs(b, Union{Tuple{Docs.Binding}, Tuple{Docs.Binding, Type}}; compare = (==)),
+        d_7 = DocSystem.getdocs(DocSystem.binding(@__MODULE__, :alias_of_getdocs), Union{Tuple{Docs.Binding}, Tuple{Docs.Binding, Type}})
 
         @test length(d_0) == 0
         @test length(d_1) == 2
@@ -59,6 +66,7 @@ const alias_of_getdocs = DocSystem.getdocs # NOTE: won't get docstrings if in a 
         @test length(d_4) == 2
         @test length(d_5) == 0
         @test length(d_6) == 1
+        @test length(d_7) == 1
 
         @test d_1[1].data[:binding] == b
         @test d_1[2].data[:binding] == b
@@ -68,18 +76,40 @@ const alias_of_getdocs = DocSystem.getdocs # NOTE: won't get docstrings if in a 
         @test d_1[2].data[:module]  == DocSystem
 
         @test d_2[1].data[:binding] == b
-        @test d_2[1].data[:typesig] == getdocs_signature
+        @test d_2[1].data[:typesig] == Union{Tuple{Any}, Tuple{Any, Type}}
         @test d_2[1].data[:module]  == DocSystem
+
+        @test d_6[1].data[:binding] == b
+        @test d_6[1].data[:typesig] == Union{Tuple{Docs.Binding}, Tuple{Docs.Binding, Type}}
+        @test d_6[1].data[:module]  == DocSystem
 
         @test d_1 == d_4
         @test d_1 != d_5
-        @test d_2 == d_6
+        @test d_6 == d_7
     end
 
     ## `UnionAll`
     let b = DocSystem.binding(@__MODULE__, Meta.parse("f(x::T) where T"))
         @test b.var == :f
     end
+
+    # DocstringAliasTest
+    a_1 = DocSystem.getdocs(Docs.Binding(DocstringAliasTest, :A))
+    a_2 = DocSystem.getdocs(Docs.Binding(DocstringAliasTest, :A), Union{})
+    a_3 = DocSystem.getdocs(Docs.Binding(DocstringAliasTest, :A), Tuple{Any})
+    b_1 = DocSystem.getdocs(Docs.Binding(DocstringAliasTest, :B))
+    b_2 = DocSystem.getdocs(Docs.Binding(DocstringAliasTest, :B), Union{})
+    b_3 = DocSystem.getdocs(Docs.Binding(DocstringAliasTest, :B), Tuple{Any})
+    @test length(a_2) == 1
+    @test a_2[1].data[:typesig] == Union{}
+    # No signature fetches the docstring of the type (Union{}) in this case
+    @test a_1 == a_2
+    @test length(a_3) == 1
+    @test a_3[1].data[:typesig] == Tuple{Any}
+    # Make sure that for an alias we get consistent docstrings
+    @test b_1 == a_1
+    @test b_2 == a_2
+    @test b_3 == a_3
 end
 
 end
