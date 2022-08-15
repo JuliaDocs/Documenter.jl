90e9ec0007464c46cdc6f26467c372e1b578e59c
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-08-16 09:18:30 +1200

    [temp] add MarkdownAST URL

--- docs/instantiate.jl
+++ docs/instantiate.jl
@@ -4,6 +4,7 @@ documenter_directory = joinpath(@__DIR__, "..")
 project_directory = isempty(ARGS) ? @__DIR__() : joinpath(pwd(), ARGS[1])
 cd(project_directory) do
     Pkg.activate(project_directory)
+    Pkg.add(url="https://github.com/JuliaDocs/MarkdownAST.jl.git")
     # Install a DocumenterTools version that declares compatibility with Documenter 0.28,
     # but from an unreleased tag.
     # https://github.com/JuliaDocs/DocumenterTools.jl/releases/tag/documenter-v0.1.14%2B0.28.0-DEV
