2d60b3f7b5bc2f16c84b74e0371e6b0764408ce5
Morten Piibeleht <morten.piibeleht@gmail.com>
2022-09-27 14:58:30 +1300

    Handle repo=nothing when generating issue references

--- src/CrossReferences.jl
+++ src/CrossReferences.jl
@@ -256,7 +256,7 @@ getsig(λ::Union{Function, DataType}, typesig) = Base.tuple_type_tail(which(λ, 
 
 function issue_xref(link::Markdown.Link, num, meta, page, doc)
     # Update issue links starting with a hash, but only if our Remote supports it
-    issue_url = Remotes.issueurl(doc.user.remote, num)
+    issue_url = isnothing(doc.user.remote) ? nothing : Remotes.issueurl(doc.user.remote, num)
     if isnothing(issue_url)
         @docerror(doc, :cross_references, "unable to generate issue reference for '[`#$num`](@ref)' in $(Utilities.locrepr(page.source)).")
     else
