cc9ff2dd9b03b7e518551f670fa1f011e60fbc6d
MarkNahabedian <naha@mit.edu>
2022-12-14 10:54:26 -0500

    Missing ).  Backtrace for @eval blocks.

--- src/Expanders.jl
+++ src/Expanders.jl
@@ -599,7 +599,7 @@ function Selectors.runner(::Type{EvalBlocks}, node, page, doc)
                     ```$(x.info)
                     $(x.code)
                     ```
-                    """, exception = err)
+                    """, exception = (err, catch_backtrace()))
             end
         end
         result = if isnothing(result)
@@ -882,7 +882,7 @@ function Selectors.runner(::Type{SetupBlocks}, node, page, doc)
             ```$(x.info)
             $(x.code)
             ```
-            """, exception=(err, catch_backtrace())
+            """, exception=(err, catch_backtrace()))
     end
     node.element = Documenter.SetupNode(x.info, x.code)
 end
