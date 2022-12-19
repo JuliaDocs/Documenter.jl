6d6eda0c8bf388bb1aa92fd0bf7002c35ce2075c
hannahilea <hannahilea@users.noreply.github.com>
2022-12-18 20:37:48 -0500

    clean up test

--- test/deploydocs.jl
+++ test/deploydocs.jl
@@ -50,11 +50,19 @@ Documenter.authenticated_repo_url(c::TestDeployConfig) = c.repo_path
                 devbranch = "master",
                 archive = joinpath(pwd(), "ghpages.tar.gz"),
             )
+            # Deploy 3.0.0 tag with a tag_prefix---which does not change deployment behavior
+            @quietly deploydocs(;
+                root = pwd(),
+                deploy_config = TestDeployConfig(full_repo_path, "3.0"),
+                repo = full_repo_path,
+                devbranch = "master",
+                tag_prefix = "MySubPackage-",
+            )
             # Check what we have in gh-pages now:
             run(`$(git()) clone -q -b gh-pages $(full_repo_path) worktree`)
             @test isfile(joinpath("worktree", "index.html"))
             @test isfile(joinpath("worktree", "versions.js"))
-            for d in ["dev", "v1.0.0", "v1.1.0"]
+            for d in ["dev", "v1.0.0", "v1.1.0", "v3.0.0"]
                 @test isfile(joinpath("worktree", d, "page.html"))
                 @test isfile(joinpath("worktree", d, "siteinfo.js"))
             end
@@ -63,72 +71,18 @@ Documenter.authenticated_repo_url(c::TestDeployConfig) = c.repo_path
             @test islink(joinpath("worktree", "v1.1"))
             @test islink(joinpath("worktree", "stable"))
             # And make sure that archived option didn't modify gh-pages
-            @test ! ispath(joinpath("worktree", "2.0.0"))
-            @test ! ispath(joinpath("worktree", "v2.0"))
-            @test ! ispath(joinpath("worktree", "v2"))
+            @test !ispath(joinpath("worktree", "2.0.0"))
+            @test !ispath(joinpath("worktree", "v2.0"))
+            @test !ispath(joinpath("worktree", "v2"))
             @test isfile("ghpages.tar.gz")
-
-            #####
-            # Repeat same set of tests, this time with a non-empty tag_prefix
-            #####
-
-            tag_prefix = "MySubPackage-"
-            # Create gh-pages and deploy dev/
-            @quietly deploydocs(;
-                root = pwd(),
-                deploy_config = TestDeployConfig(full_repo_path, tag_prefix *"dev"),
-                repo = full_repo_path,
-                devbranch = "master",
-                tag_prefix,
-            )
-            # Deploy 1.0.0 tag
-            @quietly deploydocs(;
-                root = pwd(),
-                deploy_config = TestDeployConfig(full_repo_path, tag_prefix * "1.0.0"),
-                repo = full_repo_path,
-                devbranch = "master",
-                tag_prefix,
-            )
-            # Deploy 1.1.0 tag
-            @quietly deploydocs(;
-                root = pwd(),
-                deploy_config = TestDeployConfig(full_repo_path, tag_prefix * "1.1.0"),
-                repo = full_repo_path,
-                devbranch = "master",
-                tag_prefix,
-            )
-            # Deploy 2.0.0 tag, but into an archive (so nothing pushed to gh-pages)
-            @quietly deploydocs(;
-                root = pwd(),
-                deploy_config = TestDeployConfig(full_repo_path, tag_prefix * "2.0.0"),
-                repo = full_repo_path,
-                devbranch = "master",
-                archive = joinpath(pwd(), tag_prefix * "ghpages.tar.gz"),
-                tag_prefix,
-            )
-            # Check what we have in gh-pages now:
-            cd("worktree") do
-                run(`$(git()) pull`)
-            end
-
-            for d in tag_prefix .* ["dev", "v1.0.0", "v1.1.0"]
-                @test isfile(joinpath("worktree", d, "page.html"))
-                @test isfile(joinpath("worktree", d, "siteinfo.js"))
-            end
-            @test islink(joinpath("worktree", tag_prefix * "v1"))
-            @test islink(joinpath("worktree", tag_prefix * "v1.0"))
-            @test islink(joinpath("worktree", tag_prefix * "v1.1"))
-            @test islink(joinpath("worktree", tag_prefix * "stable"))
-            # And make sure that archived option didn't modify gh-pages
-            @test ! ispath(joinpath("worktree", tag_prefix * "2.0.0"))
-            @test ! ispath(joinpath("worktree", tag_prefix * "v2.0"))
-            @test ! ispath(joinpath("worktree", tag_prefix * "v2"))
-            @test isfile(tag_prefix * "ghpages.tar.gz")
-
-            # TODO: test contents to ensure both tag_prefix and non are available
-            # (Spoiler: they currently aren't, b/c that hasn't been implemented....)
-            # @test isfile(joinpath("worktree", "index.html"))
-            # @test isfile(joinpath("worktree", "versions.js"))
+            @test islink(joinpath("worktree", "v3"))
+            @test islink(joinpath("worktree", "v3.0"))
+            @test islink(joinpath("worktree", "v3.0.0"))
+           
+            # key_prefix does not affect/is not present in worktree directories
+            @test issetequal([".git", "1.0.0", "1.1.0", "3.0", "dev", "index.html", 
+                              "stable", "v1", "v1.0", "v1.0.0", "v1.1", "v1.1.0", 
+                              "v3", "v3.0", "v3.0.0", "versions.js"], readdir("worktree"))
         end
     end
 end
