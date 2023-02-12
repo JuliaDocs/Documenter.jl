6bd93393db19f708d23f903b0cc9602f60b59f00
Documenter.jl <documenter@juliadocs.github.io>
2023-02-12 18:46:16 +0000

    build based on 735eee7

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -175,6 +175,28 @@ $(document).ready(function() {
 
 })
 ////////////////////////////////////////////////////////////////////////////////
+require([], function() {
+let searchbox = document.querySelector("#documenter-search-query");
+let sidebar = document.querySelector(".docs-sidebar");
+
+document.addEventListener("keydown", (event) => {
+  if ((event.ctrlKey || event.metaKey) && event.key === "/") {
+    if (!sidebar.classList.contains("visible")) {
+      sidebar.classList.add("visible");
+    }
+    searchbox.focus();
+    return false;
+  } else if (event.key === "Escape") {
+    if (sidebar.classList.contains("visible")) {
+      sidebar.classList.remove("visible");
+    }
+    searchbox.blur();
+    return false;
+  }
+});
+
+})
+////////////////////////////////////////////////////////////////////////////////
 require(['jquery'], function($) {
 
 // Manages the showing and hiding of the sidebar.
