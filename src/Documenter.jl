f0d5cd26ad5c65efee5b38db55920ff1a44d9fdb
Kristoffer Carlsson <kcarlsson89@gmail.com>
2023-03-06 01:26:25 +0100

    Add a basic precompile workload (#2004)

--- src/Documenter.jl
+++ src/Documenter.jl
@@ -97,4 +97,9 @@ include("deployconfig.jl")
 include("deploydocs.jl")
 include("doctest.jl")
 
+using SnoopPrecompile
+@precompile_all_calls begin
+    include("docs_precompile/make.jl")
+end
+
 end # module
---
+++ src/docs_precompile/make.jl
@@ -0,0 +1,11 @@
+using Documenter, Logging
+
+with_logger(NullLogger()) do
+makedocs(
+    sitename = "TestPkg",
+    pages = Any[
+        "Home" => "index.md",
+    ],
+    build = mktempdir()
+)
+end
