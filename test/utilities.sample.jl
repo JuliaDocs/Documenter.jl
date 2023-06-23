0196a70ceabc8a7ba970b68ad944b8bb9cbcdfad
Morten Piibeleht <morten.piibeleht@juliahub.com>
2023-06-23 15:00:43 +1200

    chore(tests): move weird character to a separate file

---
+++ test/utilities.sample.jl
@@ -0,0 +1,4 @@
+# This string is defined in a separate file so that VS Code would not
+# complain (or even accidentally "fix") the weird character we want
+# to have present every time you open utilities.jl.
+const U2028_STRING = "policy to  delete."
