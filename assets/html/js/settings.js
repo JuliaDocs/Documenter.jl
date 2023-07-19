eb87642e5a6c1bb671261cae367bcb2203543f79
Morten Piibeleht <morten.piibeleht@gmail.com>
2023-07-19 17:02:32 +1200

    Add a prettier workflow to format JS (#2186)

--- assets/html/js/settings.js
+++ assets/html/js/settings.js
@@ -2,17 +2,17 @@
 // arguments: $
 
 // Modal settings dialog
-$(document).ready(function() {
-  var settings = $('#documenter-settings');
-  $('#documenter-settings-button').click(function(){
-    settings.toggleClass('is-active');
+$(document).ready(function () {
+  var settings = $("#documenter-settings");
+  $("#documenter-settings-button").click(function () {
+    settings.toggleClass("is-active");
   });
   // Close the dialog if X is clicked
-  $('#documenter-settings button.delete').click(function(){
-    settings.removeClass('is-active');
+  $("#documenter-settings button.delete").click(function () {
+    settings.removeClass("is-active");
   });
   // Close dialog if ESC is pressed
-  $(document).keyup(function(e) {
-    if (e.keyCode == 27) settings.removeClass('is-active');
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) settings.removeClass("is-active");
   });
 });
