b89e12abfc80168be4b3740bfe33db70df27ffed
Documenter.jl <documenter@juliadocs.github.io>
2023-07-19 05:04:43 +0000

    build based on eb87642

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -99,7 +99,9 @@ $(document).on("click", ".docstring header", function () {
     articleToggleTitle = "Collapse docstring";
   }
 
-  $(this).find(".docstring-article-toggle-button").prop("title", articleToggleTitle);
+  $(this)
+    .find(".docstring-article-toggle-button")
+    .prop("title", articleToggleTitle);
   $(this).siblings("section").slideToggle();
 });
 
@@ -204,29 +206,29 @@ require(['jquery', 'headroom', 'headroom-jquery'], function($, Headroom) {
 // Manages the top navigation bar (hides it when the user starts scrolling down on the
 // mobile).
 window.Headroom = Headroom; // work around buggy module loading?
-$(document).ready(function() {
-  $('#documenter .docs-navbar').headroom({
-    "tolerance": {"up": 10, "down": 10},
+$(document).ready(function () {
+  $("#documenter .docs-navbar").headroom({
+    tolerance: { up: 10, down: 10 },
   });
-})
+});
 
 })
 ////////////////////////////////////////////////////////////////////////////////
 require(['jquery'], function($) {
 
 // Modal settings dialog
-$(document).ready(function() {
-  var settings = $('#documenter-settings');
-  $('#documenter-settings-button').click(function(){
-    settings.toggleClass('is-active');
+$(document).ready(function () {
+  var settings = $("#documenter-settings");
+  $("#documenter-settings-button").click(function () {
+    settings.toggleClass("is-active");
   });
   // Close the dialog if X is clicked
-  $('#documenter-settings button.delete').click(function(){
-    settings.removeClass('is-active');
+  $("#documenter-settings button.delete").click(function () {
+    settings.removeClass("is-active");
   });
   // Close dialog if ESC is pressed
-  $(document).keyup(function(e) {
-    if (e.keyCode == 27) settings.removeClass('is-active');
+  $(document).keyup(function (e) {
+    if (e.keyCode == 27) settings.removeClass("is-active");
   });
 });
 
@@ -257,70 +259,70 @@ document.addEventListener("keydown", (event) => {
 require(['jquery'], function($) {
 
 // Manages the showing and hiding of the sidebar.
-$(document).ready(function() {
+$(document).ready(function () {
   var sidebar = $("#documenter > .docs-sidebar");
-  var sidebar_button = $("#documenter-sidebar-button")
-  sidebar_button.click(function(ev) {
+  var sidebar_button = $("#documenter-sidebar-button");
+  sidebar_button.click(function (ev) {
     ev.preventDefault();
-    sidebar.toggleClass('visible');
-    if (sidebar.hasClass('visible')) {
+    sidebar.toggleClass("visible");
+    if (sidebar.hasClass("visible")) {
       // Makes sure that the current menu item is visible in the sidebar.
       $("#documenter .docs-menu a.is-active").focus();
     }
   });
-  $("#documenter > .docs-main").bind('click', function(ev) {
+  $("#documenter > .docs-main").bind("click", function (ev) {
     if ($(ev.target).is(sidebar_button)) {
       return;
     }
-    if (sidebar.hasClass('visible')) {
-      sidebar.removeClass('visible');
+    if (sidebar.hasClass("visible")) {
+      sidebar.removeClass("visible");
     }
   });
-})
+});
 
 // Resizes the package name / sitename in the sidebar if it is too wide.
 // Inspired by: https://github.com/davatron5000/FitText.js
-$(document).ready(function() {
+$(document).ready(function () {
   e = $("#documenter .docs-autofit");
   function resize() {
-    var L = parseInt(e.css('max-width'), 10);
+    var L = parseInt(e.css("max-width"), 10);
     var L0 = e.width();
-    if(L0 > L) {
-      var h0 = parseInt(e.css('font-size'), 10);
-      e.css('font-size', L * h0 / L0);
+    if (L0 > L) {
+      var h0 = parseInt(e.css("font-size"), 10);
+      e.css("font-size", (L * h0) / L0);
       // TODO: make sure it survives resizes?
     }
   }
   // call once and then register events
   resize();
   $(window).resize(resize);
-  $(window).on('orientationchange', resize);
+  $(window).on("orientationchange", resize);
 });
 
 // Scroll the navigation bar to the currently selected menu item
-$(document).ready(function() {
+$(document).ready(function () {
   var sidebar = $("#documenter .docs-menu").get(0);
   var active = $("#documenter .docs-menu .is-active").get(0);
-  if(typeof active !== 'undefined') {
+  if (typeof active !== "undefined") {
     sidebar.scrollTop = active.offsetTop - sidebar.offsetTop - 15;
   }
-})
+});
 
 })
 ////////////////////////////////////////////////////////////////////////////////
 require(['jquery'], function($) {
 
 // Theme picker setup
-$(document).ready(function() {
+$(document).ready(function () {
   // onchange callback
-  $('#documenter-themepicker').change(function themepick_callback(ev){
-    var themename = $('#documenter-themepicker option:selected').attr('value');
-    if (themename === 'auto') {
+  $("#documenter-themepicker").change(function themepick_callback(ev) {
+    var themename = $("#documenter-themepicker option:selected").attr("value");
+    if (themename === "auto") {
       // set_theme(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-      window.localStorage.removeItem('documenter-theme');
+      window.localStorage.removeItem("documenter-theme");
     } else {
       // set_theme(themename);
-      window.localStorage.setItem('documenter-theme', themename);
+      window.localStorage.setItem("documenter-theme", themename);
     }
     // We re-use the global function from themeswap.js to actually do the swapping.
     set_theme_from_local_storage();
@@ -328,52 +330,68 @@ $(document).ready(function() {
 
   // Make sure that the themepicker displays the correct theme when the theme is retrieved
   // from localStorage
-  if(typeof(window.localStorage) !== "undefined") {
-    var theme =  window.localStorage.getItem("documenter-theme");
-    if(theme !== null) {
-      $('#documenter-themepicker option').each(function(i,e) {
-        e.selected = (e.value === theme);
-      })
+  if (typeof window.localStorage !== "undefined") {
+    var theme = window.localStorage.getItem("documenter-theme");
+    if (theme !== null) {
+      $("#documenter-themepicker option").each(function (i, e) {
+        e.selected = e.value === theme;
+      });
     }
   }
-})
+});
 
 })
 ////////////////////////////////////////////////////////////////////////////////
 require(['jquery'], function($) {
 
 // update the version selector with info from the siteinfo.js and ../versions.js files
-$(document).ready(function() {
+$(document).ready(function () {
   // If the version selector is disabled with DOCUMENTER_VERSION_SELECTOR_DISABLED in the
   // siteinfo.js file, we just return immediately and not display the version selector.
-  if (typeof DOCUMENTER_VERSION_SELECTOR_DISABLED === 'boolean' && DOCUMENTER_VERSION_SELECTOR_DISABLED) {
+  if (
+    typeof DOCUMENTER_VERSION_SELECTOR_DISABLED === "boolean" &&
+    DOCUMENTER_VERSION_SELECTOR_DISABLED
+  ) {
     return;
   }
 
   var version_selector = $("#documenter .docs-version-selector");
   var version_selector_select = $("#documenter .docs-version-selector select");
 
-  version_selector_select.change(function(x) {
-    target_href = version_selector_select.children("option:selected").get(0).value;
+  version_selector_select.change(function (x) {
+    target_href = version_selector_select
+      .children("option:selected")
+      .get(0).value;
     window.location.href = target_href;
   });
 
   // add the current version to the selector based on siteinfo.js, but only if the selector is empty
-  if (typeof DOCUMENTER_CURRENT_VERSION !== 'undefined' && $('#version-selector > option').length == 0) {
-    var option = $("<option value='#' selected='selected'>" + DOCUMENTER_CURRENT_VERSION + "</option>");
+  if (
+    typeof DOCUMENTER_CURRENT_VERSION !== "undefined" &&
+    $("#version-selector > option").length == 0
+  ) {
+    var option = $(
+      "<option value='#' selected='selected'>" +
+        DOCUMENTER_CURRENT_VERSION +
+        "</option>"
+    );
     version_selector_select.append(option);
   }
 
-  if (typeof DOC_VERSIONS !== 'undefined') {
+  if (typeof DOC_VERSIONS !== "undefined") {
     var existing_versions = version_selector_select.children("option");
-    var existing_versions_texts = existing_versions.map(function(i,x){return x.text});
-    DOC_VERSIONS.forEach(function(each) {
+    var existing_versions_texts = existing_versions.map(function (i, x) {
+      return x.text;
+    });
+    DOC_VERSIONS.forEach(function (each) {
       var version_url = documenterBaseURL + "/../" + each + "/";
       var existing_id = $.inArray(each, existing_versions_texts);
       // if not already in the version selector, add it as a new option,
       // otherwise update the old option with the URL and enable it
       if (existing_id == -1) {
-        var option = $("<option value='" + version_url + "'>" + each + "</option>");
+        var option = $(
+          "<option value='" + version_url + "'>" + each + "</option>"
+        );
         version_selector_select.append(option);
       } else {
         var option = existing_versions[existing_id];
@@ -387,6 +405,6 @@ $(document).ready(function() {
   if (version_selector_select.children("option").length > 0) {
     version_selector.toggleClass("visible");
   }
-})
+});
 
 })
