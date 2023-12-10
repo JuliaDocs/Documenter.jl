b69e9a01382228440c0cec24fd54375eeddfe54a
Morten Piibeleht <morten.piibeleht@juliahub.com>
2023-12-10 20:25:44 +1300

    fix tests, again

--- test/base_assumptions.jl
+++ test/base_assumptions.jl
@@ -1,7 +1,12 @@
+"""
+These tests test assumptions about the Julia Base module. In particular, this will
+test the presence of the various internal, non-public functions that are used by
+Documenter.jl.
+"""
 module BaseAssumptionTests
 using Test
 
-@test "Julia Base assumptions" begin
+@testset "Julia Base assumptions" begin
     # To handle source URLs to standard library files, we need to fix up the paths to
     # standard library objects (which generally point to /cache/..., for the pre-built
     # binaries).
