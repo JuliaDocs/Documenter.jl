93e9b57cc428535da958ad1118cb28ce6ff516f4
Documenter.jl <documenter@juliadocs.github.io>
2024-01-15 22:00:33 +0000

    build based on deb6fbe

--- previews/PR2394/assets/docstringscollapsed.js
+++ previews/PR2394/assets/docstringscollapsed.js
@@ -1,8 +1,12 @@
 // Should be loaded if `DocStringsCollapse = true` in `@meta` block of page, and
 // clicks the button to toggle docstrings after the page loads.
 function clickToggleButton() {
-  var toggleButton = document.getElementById('documenter-article-toggle-button');
-  if (toggleButton) {toggleButton.click();}
+  var toggleButton = document.getElementById(
+    "documenter-article-toggle-button",
+  );
+  if (toggleButton) {
+    toggleButton.click();
+  }
 }
 
 if (document.readyState === "loading") {
