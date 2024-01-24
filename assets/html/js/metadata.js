b4ba3a7cea2fa91caa0a5f4a1b61911a871570f1
Michael Goerz <mail@michaelgoerz.net>
2024-01-23 21:28:10 -0500

    Remove the toggle animation

--- assets/html/js/metadata.js
+++ assets/html/js/metadata.js
@@ -4,5 +4,8 @@
 let meta = $("div[data-docstringscollapsed]").data();
 
 if (meta.docstringscollapsed) {
-  $("#documenter-article-toggle-button").click();
+  $("#documenter-article-toggle-button").trigger({
+    type: "click",
+    noToggleAnimation: true,
+  });
 }
