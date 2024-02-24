98a899b1a0afe7b7833a33dba42ea613b5c1e9fc
Hetarth Shah <hetarth02@gmail.com>
2024-02-24 15:01:32 +0530

    - Updated check for metadata object exists

--- assets/html/js/metadata.js
+++ assets/html/js/metadata.js
@@ -4,13 +4,10 @@
 $(document).ready(function () {
   let meta = $("div[data-docstringscollapsed]").data();
 
-  // Check if metadata object exists
-  if (meta) {
-    if (meta.docstringscollapsed) {
-      $("#documenter-article-toggle-button").trigger({
-        type: "click",
-        noToggleAnimation: true,
-      });
-    }
+  if (meta?.docstringscollapsed) {
+    $("#documenter-article-toggle-button").trigger({
+      type: "click",
+      noToggleAnimation: true,
+    });
   }
 });
