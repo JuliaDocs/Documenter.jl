3ad03377dc7ea8c6c86f6e25badc711c55aa1e67
Morten Piibeleht <morten.piibeleht@gmail.com>
2024-03-02 11:13:29 +0000

    build based on a6f6c0c

--- versions.js
+++ versions.js
@@ -1,9 +1,17 @@
 var DOC_VERSIONS = [
   "stable",
+  "v1.3",
   "v1.2",
+  "v1.1",
+  "v1.0",
+  "v0.27",
+  "v0.26",
+  "v0.25",
   "v0.24",
   "v0.23",
   "v0.22",
   "v0.21",
   "dev",
 ];
+var DOCUMENTER_NEWEST = "v1.3.0";
+var DOCUMENTER_STABLE = "stable";
