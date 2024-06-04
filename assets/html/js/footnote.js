6ce6fd91b6ff368e93994e3922ae8d538c43e41d
Shravan Goswami <shravanngoswamii@gmail.com>
2024-06-04 13:28:12 +0530

    added small jquery script for getting footnote previews from end of the page

---
+++ assets/html/js/footnote.js
@@ -0,0 +1,9 @@
+// libraries: jquery
+// arguments: $
+$(document).ready(function() {
+    $('.footnote-ref').hover(function() {
+        var id = $(this).attr('href');
+        var footnoteContent = $(id).clone().find('a').remove().end().html();
+        $(this).next('.footnote-preview').html(footnoteContent);
+    });
+});
