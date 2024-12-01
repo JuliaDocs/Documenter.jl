731a587fbeac2c2fbc40e8c08ab6590965b91a75
BambOoxX <42067365+BambOoxX@users.noreply.github.com>
2024-12-01 17:57:39 +0100

    Update latex examples list

--- test/examples/tests_latex.jl
+++ test/examples/tests_latex.jl
@@ -2,9 +2,9 @@ using Test
 
 # DOCUMENTER_TEST_EXAMPLES can be used to control which builds are performed in
 # make.jl, and we need to set it to the relevant LaTeX builds.
-ENV["DOCUMENTER_TEST_EXAMPLES"] =
+ENV["DOCUMENTER_TEST_EXAMPLES"] = 
     "latex latex_simple latex_cover_page latex_toc_style latex_simple_tectonic " *
-    "latex_showcase"
+    "latex_showcase latex_simple_nondocker"
 
 # When the file is run separately we need to include make.jl which actually builds
 # the docs and defines a few modules that are referred to in the docs. The make.jl
@@ -27,6 +27,15 @@ else
 end
 
 @testset "Examples/LaTeX" begin
+
+    @testset "PDF/LaTeX: simple nondocker" begin
+        doc = Main.examples_latex_simple_nondocker_doc
+        @test isa(doc, Documenter.Documenter.Document)
+        let build_dir = joinpath(examples_root, "builds", "latex_simple_nondocker")
+            @test joinpath(build_dir, "DocumenterLaTeXSimpleNonDocker-1.2.3.pdf") |> isfile
+        end
+    end
+
     @testset "PDF/LaTeX: simple" begin
         doc = Main.examples_latex_simple_doc
         @test isa(doc, Documenter.Documenter.Document)
