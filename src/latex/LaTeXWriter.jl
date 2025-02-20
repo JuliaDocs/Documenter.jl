c12e4477138de7a4d4bb26f42062553aba032a0d
Morten Piibeleht <morten.piibeleht@gmail.com>
2025-02-21 10:47:18 +1300

    Update src/latex/LaTeXWriter.jl

--- src/latex/LaTeXWriter.jl
+++ src/latex/LaTeXWriter.jl
@@ -107,7 +107,7 @@ const DOCUMENT_STRUCTURE = (
 )
 
 function render(doc::Documenter.Document, settings::LaTeX = LaTeX())
-    if isempty(doc.users.sitename) # otherwise, the latex compiler will terminate with a cryptic "There's no line here to end" error
+    if isempty(doc.user.sitename) # otherwise, the latex compiler will terminate with a cryptic "There's no line here to end" error
         error("""
             LaTeXWriter needs a non-empty `sitename` passed to `makedocs`, otherwise the LaTeX build will error!  
             Please pass e.g. `sitename = "Some Site Name"` as a keyword argument to `makedocs`.
