7d7711408c1d08b99852fab10fd87e60b7816858
Documenter.jl <documenter@juliadocs.github.io>
2025-03-11 23:00:38 +0000

    build based on 3e1437e

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -245,6 +245,20 @@ $(document).ready(function () {
       type: "click",
       noToggleAnimation: true,
     });
+
+    setTimeout(function () {
+      if (window.location.hash) {
+        const targetId = window.location.hash.substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+          });
+        }
+      }
+    }, 100);
   }
 });
 
