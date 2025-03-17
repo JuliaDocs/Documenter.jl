44c47841c28ddd37f7b959ba5256d14a518460cb
Max Horn <max@quendi.de>
2025-03-17 13:51:31 +0100

    make it relative to initial pwd

--- test/docsxref/make.jl
+++ test/docsxref/make.jl
@@ -56,7 +56,7 @@ end
     @test contains(
         replace(captured.output, "src\\index" => "src/index"),
         """
-        ┌ Warning: Cannot resolve @ref for md"[`AbstractSelector`](@ref)" in src/index.md.
+        ┌ Warning: Cannot resolve @ref for md"[`AbstractSelector`](@ref)" in docsxref/src/index.md.
         │ - No docstring found in doc for binding `Main.DocsReferencingMain.AbstractSelector`.
         │ - Fallback resolution in Main for `AbstractSelector` -> `Documenter.Selectors.AbstractSelector` is only allowed for fully qualified names
         """
@@ -64,7 +64,7 @@ end
     @test contains(
         replace(captured.output, "src\\page" => "src/page"),
         """
-        ┌ Warning: Cannot resolve @ref for md"[`DocsReferencingMain.f`](@ref)" in src/page.md.
+        ┌ Warning: Cannot resolve @ref for md"[`DocsReferencingMain.f`](@ref)" in docsxref/src/page.md.
         │ - Exception trying to find docref for `DocsReferencingMain.f`: unable to get the binding for `DocsReferencingMain.f` in module Documenter.Selectors
         │ - Fallback resolution in Main for `DocsReferencingMain.f` -> `Main.DocsReferencingMain.f` is only allowed for fully qualified names
         """
