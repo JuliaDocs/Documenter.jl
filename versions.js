b44b3d7ad7504d8710f8f0c53ba6f4c5364a8ec8
Morten Piibeleht <morten.piibeleht@gmail.com>
2025-04-01 01:40:02 +0000

    build based on 4328aec

--- versions.js
+++ versions.js
@@ -1,9 +1,24 @@
 var DOC_VERSIONS = [
   "stable",
+  "v1.10",
+  "v1.9",
+  "v1.8",
+  "v1.7",
+  "v1.6",
+  "v1.5",
+  "v1.4",
+  "v1.3",
   "v1.2",
+  "v1.1",
+  "v1.0",
+  "v0.27",
+  "v0.26",
+  "v0.25",
   "v0.24",
   "v0.23",
   "v0.22",
   "v0.21",
   "dev",
 ];
+var DOCUMENTER_NEWEST = "v1.10.1";
+var DOCUMENTER_STABLE = "stable";
