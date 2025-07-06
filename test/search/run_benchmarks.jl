0f32b37729b9c9c8b87a6d016d96e6ae149776f7
rahban <rahban.ghani2001@gmail.com>
2025-07-06 14:32:54 +0530

    now the values are compared to a fix set of values everytime it runs

--- test/search/run_benchmarks.jl
+++ test/search/run_benchmarks.jl
@@ -2,10 +2,21 @@ using Statistics
 using Dates
 using PrettyTables
 using Crayons
+using JSON
 
 include(joinpath(@__DIR__, "test_queries.jl"))
 include(joinpath(@__DIR__, "evaluate.jl"))
 include(joinpath(@__DIR__, "real_search.jl"))
+
+# Function to load reference benchmark values
+function load_reference_values()
+    reference_file = joinpath(@__DIR__, "search_benchmark_reference.json")
+    if isfile(reference_file)
+        return JSON.parsefile(reference_file)
+    else
+        return Dict()
+    end
+end
 
 function write_detailed_results(results::EvaluationResults, category::String, io::IO)
     println(io, "\n== $category ==")
@@ -38,6 +49,9 @@ end
 
 function run_benchmarks()
     println("Running search benchmarks...")
+
+    # Load reference values
+    reference_values = load_reference_values()
 
     # Test navigational queries
     navigational_results = evaluate_all(real_search, navigational_queries)
@@ -78,23 +92,55 @@ function run_benchmarks()
     recall_val = round(all_results.average_recall * 100, digits = 1)
     f1_val = round(all_results.average_f1_score * 100, digits = 1)
 
+    # Calculate differences from reference values
+    precision_ref = get(reference_values, "average_precision", 0.0)
+    recall_ref = get(reference_values, "average_recall", 0.0)
+    f1_ref = get(reference_values, "average_f1_score", 0.0)
+
+    precision_diff = precision_val - precision_ref
+    recall_diff = recall_val - recall_ref
+    f1_diff = f1_val - f1_ref
+
+    # Function to get color for differences
+    function get_color_for_diff(diff)
+        if diff > 0
+            return crayon"green"
+        elseif diff < 0
+            return crayon"red"
+        else
+            return crayon"default"
+        end
+    end
+
     summary_data = [
-        string("Average Precision")  string(precision_val);
-        string("Average Recall")     string(recall_val);
-        string("Average F1 Score")   string(f1_val);
-        string("Total Relevant Found") string(all_results.total_relevant_found);
-        string("Total Documents Retrieved") string(all_results.total_documents_retrieved);
-        string("Total Relevant Documents") string(all_results.total_relevant_documents)
+        "Average Precision" precision_val precision_ref string(round(precision_diff, digits=1));
+        "Average Recall" recall_val recall_ref string(round(recall_diff, digits=1));
+        "Average F1 Score" f1_val f1_ref string(round(f1_diff, digits=1));
+        "Total Relevant Found" all_results.total_relevant_found "" "";
+        "Total Documents Retrieved" all_results.total_documents_retrieved "" "";
+        "Total Relevant Documents" all_results.total_relevant_documents "" ""
     ]
 
     # Create highlighters for each percentage metric
     precision_highlighter = Highlighter((data, i, j) -> i == 1 && j == 2, get_color_for_percentage(precision_val))
     recall_highlighter = Highlighter((data, i, j) -> i == 2 && j == 2, get_color_for_percentage(recall_val))
     f1_highlighter = Highlighter((data, i, j) -> i == 3 && j == 2, get_color_for_percentage(f1_val))
+    diff_highlighter = Highlighter(
+        (data, i, j) -> j == 4 && i in 1:3,
+        (data, i, j) -> get_color_for_diff(parse(Float64, data[i, 4]))
+    )
 
     pretty_table(
-        summary_data, header = ["Metric", "Value"]; alignment = :l,
-        header_crayon = crayon"bold blue", highlighters = (precision_highlighter, recall_highlighter, f1_highlighter)
+        summary_data, header = ["Metric", "Value", "Reference", "Diff"], alignment = :l,
+        header_crayon = crayon"bold blue",
+        highlighters = (
+            precision_highlighter,
+            recall_highlighter,
+            f1_highlighter,
+            Highlighter((data, i, j) -> j == 4 && i in 1:3, get_color_for_diff(parse(Float64, summary_data[1, 4]))),
+            Highlighter((data, i, j) -> j == 4 && i in 1:3, get_color_for_diff(parse(Float64, summary_data[2, 4]))),
+            Highlighter((data, i, j) -> j == 4 && i in 1:3, get_color_for_diff(parse(Float64, summary_data[3, 4])))
+        )
     )
 
     # Write detailed results to file
