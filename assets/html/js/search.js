f4eaeda82e07e8dd2c0feda4fe83db7363ccaa48
rahban <rahban.ghani2001@gmail.com>
2025-07-11 01:29:12 +0530

    Ran prettier

--- assets/html/js/search.js
+++ assets/html/js/search.js
@@ -606,7 +606,10 @@ function waitUntilSearchIndexAvailable() {
   // has finished loading and documenterSearchIndex gets defined.
   // So we need to wait until the search index actually loads before setting
   // up all the search-related stuff.
-  if (typeof documenterSearchIndex !== "undefined" && typeof $ !== 'undefined') {
+  if (
+    typeof documenterSearchIndex !== "undefined" &&
+    typeof $ !== "undefined"
+  ) {
     runSearchMainCode();
   } else {
     console.warn("Search Index or jQuery not available, waiting");
