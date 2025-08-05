b434be517f7fbd4b818179d3cb6f097d2ac7a6c9
rahban <rahban.ghani2001@gmail.com>
2025-08-05 11:16:40 +0530

    fixes CI

--- test/search/run_all_benchmarks.jl
+++ test/search/run_all_benchmarks.jl
@@ -1,5 +1,5 @@
 println("Running benchmarks for test_queries.jl...")
-run(`julia $(@__DIR__)/run_benchmarks.jl $(@__DIR__)/../../docs/build/search_index.js $(@__DIR__)/test_queries.jl all_test_queries`)
+run(`julia --project=$(@__DIR__) $(@__DIR__)/run_benchmarks.jl $(@__DIR__)/../../docs/build/search_index.js $(@__DIR__)/test_queries.jl all_test_queries`)
 
 println("Running benchmarks for edge_case_queries.jl...")
-run(`julia $(@__DIR__)/run_benchmarks.jl $(@__DIR__)/../search_edge_cases/build/search_index.js $(@__DIR__)/edge_case_queries.jl all_edge_case_queries`)
+run(`julia --project=$(@__DIR__) $(@__DIR__)/run_benchmarks.jl $(@__DIR__)/../search_edge_cases/build/search_index.js $(@__DIR__)/edge_case_queries.jl all_edge_case_queries`)
