8f5f87793bf6a73e1f3ac051127a492feda24883
Aaruni Kaushik <aaruni@edufor.me>
2025-10-21 16:42:28 +0200

    linting: run prettier

--- assets/html/js/collapse.js
+++ assets/html/js/collapse.js
@@ -1,16 +1,14 @@
 ///////////////////////////////////
 function openTarget() {
-    const hash = decodeURIComponent(location.hash.substring(1));
-    if (hash) {
-        const target = document.getElementById(hash);
-        if (target) {
-            const details = target.closest('details');
-            if (details)
-                details.open = true;
-        }
+  const hash = decodeURIComponent(location.hash.substring(1));
+  if (hash) {
+    const target = document.getElementById(hash);
+    if (target) {
+      const details = target.closest("details");
+      if (details) details.open = true;
     }
+  }
 }
 openTarget(); // onload
-window.addEventListener('hashchange', openTarget);
-window.addEventListener('load', openTarget);
-
+window.addEventListener("hashchange", openTarget);
+window.addEventListener("load", openTarget);
