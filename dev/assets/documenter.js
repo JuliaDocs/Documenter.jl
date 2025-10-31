93b11119f00835905fcf539b45287b74fa4f4e15
Documenter.jl <documenter@juliadocs.github.io>
2025-10-31 15:06:36 +0000

    build based on 1d8b68f

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -203,6 +203,57 @@ if (document.readyState === "loading") {
 
 })
 ////////////////////////////////////////////////////////////////////////////////
+require(['jquery'], function($) {
+$(document).ready(function () {
+  $(".footnote-ref").hover(
+    function () {
+      var id = $(this).attr("href");
+      var footnoteContent = $(id).clone().find("a").remove().end().html();
+
+      var $preview = $(this).next(".footnote-preview");
+
+      $preview.html(footnoteContent).css({
+        display: "block",
+        left: "50%",
+        transform: "translateX(-50%)",
+      });
+
+      repositionPreview($preview, $(this));
+    },
+    function () {
+      var $preview = $(this).next(".footnote-preview");
+      $preview.css({
+        display: "none",
+        left: "",
+        transform: "",
+        "--arrow-left": "",
+      });
+    },
+  );
+
+  function repositionPreview($preview, $ref) {
+    var previewRect = $preview[0].getBoundingClientRect();
+    var refRect = $ref[0].getBoundingClientRect();
+    var viewportWidth = $(window).width();
+
+    if (previewRect.right > viewportWidth) {
+      var excessRight = previewRect.right - viewportWidth;
+      $preview.css("left", `calc(50% - ${excessRight + 10}px)`);
+    } else if (previewRect.left < 0) {
+      var excessLeft = 0 - previewRect.left;
+      $preview.css("left", `calc(50% + ${excessLeft + 10}px)`);
+    }
+
+    var newPreviewRect = $preview[0].getBoundingClientRect();
+
+    var arrowLeft = refRect.left + refRect.width / 2 - newPreviewRect.left;
+
+    $preview.css("--arrow-left", arrowLeft + "px");
+  }
+});
+
+})
+////////////////////////////////////////////////////////////////////////////////
 require(['jquery', 'headroom', 'headroom-jquery'], function($, Headroom) {
 
 // Manages the top navigation bar (hides it when the user starts scrolling down on the
