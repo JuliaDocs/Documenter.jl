2bac6f50006c652fd13191a75067de5f15dd9516
Anshul Singhvi <asinghvi17@simons-rock.edu>
2025-11-07 05:59:32 -0500

    Don't use the Module(...) constructor to create a module (#2683)

--- src/utilities/utilities.jl
+++ src/utilities/utilities.jl
@@ -628,12 +628,19 @@ function get_sandbox_module!(meta, prefix, name = nothing; share_default_module 
     # Either fetch and return an existing sandbox from the meta dictionary (based on the generated name),
     # or initialize a new clean one, which gets stored in meta for future re-use.
     return get!(meta, sym) do
-        # If the module does not exists already, we need to construct a new one.
-        m = Module(sym)
-        # eval(expr) is available in the REPL (i.e. Main) so we emulate that for the sandbox
-        Core.eval(m, :(eval(x) = Core.eval($m, x)))
-        # modules created with Module() does not have include defined
-        Core.eval(m, :(include(x) = Base.include($m, abspath(x))))
+        # If the module does not exist already, we need to construct a new one.
+        # We create a baremodule so that we can insert a custom `include` method
+        # that is closer to the one in Main, in that it works relative to the
+        # current working directory, not relative to the module.
+        m = Core.eval(
+            Main, :(
+                baremodule $sym
+                using Base
+                eval(x) = Core.eval($sym, x)
+                include(x) = Base.include($sym, abspath(x))
+                end
+            )
+        )
         return m
     end
 end
