b48b0f8d9111fb977df085b0f70f946719a7709c
Max Horn <max@quendi.de>
2025-11-07 13:13:13 +0100

    seems runic is not perfect

--- src/latex/LaTeXWriter.jl
+++ src/latex/LaTeXWriter.jl
@@ -852,15 +852,15 @@ latex(io::Context, node::Node, ::Documenter.SetupNode) = nothing
 function latex(io::Context, node::Node, value::MarkdownAST.JuliaValue)
     message =
     """
-        Unexpected Julia interpolation in the Markdown. This probably means that you have an
-        unbalanced or un-escaped \$ in the text.
+    Unexpected Julia interpolation in the Markdown. This probably means that you have an
+    unbalanced or un-escaped \$ in the text.
 
-        To write the dollar sign, escape it with `\\\$`
+    To write the dollar sign, escape it with `\\\$`
 
-        This is in file $(io.filename), and we were given the value:
+    This is in file $(io.filename), and we were given the value:
 
-        `$(value.ref)` which is of type `$(typeof(value.ref))`
-        """
+    `$(value.ref)` which is of type `$(typeof(value.ref))`
+    """
     if io.doc.user.treat_markdown_warnings_as_error
         error(message)
     else
