6b23d7c2b17d636b9e9c249e921e7569f85bbd63
Max Horn <max@quendi.de>
2025-11-07 16:39:54 +0100

    Run prettier

--- assets/html/js/footnote.js
+++ assets/html/js/footnote.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
         transform: "",
         "--arrow-left": "",
       });
-    },
+    }
   );
 
   function repositionPreview($preview, $ref) {
--- assets/html/js/shortcut.js
+++ assets/html/js/shortcut.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
           ${search_modal_footer}
         </div>
       </div>
-    `,
+    `
   );
 
   function checkURLForSearch() {
